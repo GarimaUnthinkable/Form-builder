81014f6850d72c584e2500dbe8102f353278a0f8
GarimaUnthinkable <garima@unthinkable.co>
2022-11-18 16:26:32 +0530

    created

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+
+  form: any[] = [];
 
   ngOnInit(): void {
   }
